a5cba21c2e80993f2ed45c2a5064b95bc53a4971
trevd <trevd1234@gmail.com>
2013-02-18 08:20:25 +0000

    Build for mac on linux!! That is all

---
+++ core/envsetup.mk
@@ -0,0 +1,10 @@
+# Under Linux, if USE_MINGW is set, google change HOST_OS to Windows to build the
+# Windows SDK. Only a subset of tools and SDK will manage to build properly.
+
+# And we'll do exactly the same trick for darwin!!!
+# Thanks very much
+ifeq ($(HOST_OS),linux)
+ifneq ($(USE_DARWIN),)
+	HOST_OS := darwin
+endif
+endif
