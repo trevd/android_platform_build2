d5a30874a61e79ec67aac686e40f679380d169e3
trevd <trevdrake@hotmail.com>
2013-02-26 09:24:27 +0000

    Update envsetup.sh

--- envsetup.sh
+++ envsetup.sh
@@ -1,7 +1,6 @@
-
 function initialize_ccache(){
 	export USE_CCACHE=1
-	mkdir $PWD/.ccache
+	mkdir -p $PWD/.ccache
 	export CCACHE_DIR=$PWD/.ccache
 	prebuilts/misc/linux-x86/ccache/ccache -M 50G
 }
@@ -10,4 +9,10 @@ unset ANDROID_BUILD_PATHS
 source build/envsetup.sh
 echo $ANDROID_BUILD_PATHS
 alias mmwin='USE_MINGW=true mm'
+alias mmmac='USE_DARWIN=true mm'
+alias mmpi='USE_PI=true mm'
 initialize_ccache
+
+export MAC_SDKROOT=$(dirname `which i686-apple-darwin10-gcc`)
+export MAC_SDK_VERSION="10.6"
+
