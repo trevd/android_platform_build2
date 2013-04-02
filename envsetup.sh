95d814a25f42aae6b65541f233ef9fb511bce964
trevd <trevd1234@gmail.com>
2013-04-02 07:11:17 +0100

    patch build files for cross compiling osx on linux

--- envsetup.sh
+++ envsetup.sh
@@ -13,5 +13,5 @@ alias mmmac='USE_DARWIN=true mm'
 alias mmpi='USE_PI=true mm'
 initialize_ccache
 
-export MAC_SDKROOT=$(dirname `which i686-apple-darwin10-gcc`)
+#export MAC_SDKROOT=$(dirname `which i686-apple-darwin10-gcc`)
 export MAC_SDK_VERSION="10.6"
