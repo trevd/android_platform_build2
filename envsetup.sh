f93b10695669e639595efc805a8d585100ac79a5
Trevor Drake <trevd1234@gmail.com>
2013-05-29 02:47:28 +0100

    no ccache

--- envsetup.sh
+++ envsetup.sh
@@ -1,8 +1,8 @@
 function initialize_ccache(){
-	export USE_CCACHE=1
-	mkdir -p $PWD/.ccache
-	export CCACHE_DIR=$PWD/.ccache
-	prebuilts/misc/linux-x86/ccache/ccache -M 50G
+    export USE_CCACHE=1
+    mkdir -p $PWD/.ccache
+    export CCACHE_DIR=$PWD/.ccache
+    prebuilts/misc/linux-x86/ccache/ccache -M 50G
 }
 echo "Running build2"
 unset ANDROID_BUILD_PATHS
@@ -11,7 +11,7 @@ echo $ANDROID_BUILD_PATHS
 alias mmwin='USE_MINGW=true mm'
 alias mmmac='USE_DARWIN=true mm'
 alias mmpi='USE_PI=true mm'
-initialize_ccache
+#initialize_ccache
 
 #export MAC_SDKROOT=$(dirname `which i686-apple-darwin10-gcc`)
 #export MAC_SDK_VERSION="10.6"
